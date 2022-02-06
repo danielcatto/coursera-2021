bf3fee080855d5c873498958a7a194b1e431cabf
danielcatto <danielcatto@hotmail.com>
2022-02-06 15:56:32 -0300

    implementando def avalia_textos()

--- semana9/coh-piah.py
+++ semana9/coh-piah.py
@@ -5,12 +5,12 @@ def le_assinatura():
     print("Bem-vindo ao detector automático de COH-PIAH.")
     print("Informe a assinatura típica de um aluno infectado:")
 
-    wal = 4.51#float(input("Entre o tamanho médio de palavra:"))
-    ttr = 0.693#float(input("Entre a relação Type-Token:"))
-    hlr = 0.55#float(input("Entre a Razão Hapax Legomana:"))
-    sal = 70.82#float(input("Entre o tamanho médio de sentença:"))
-    sac = 1.82#float(input("Entre a complexidade média da sentença:"))
-    pal = 38.5#float(input("Entre o tamanho medio de frase:"))
+    wal = float(input("Entre o tamanho médio de palavra:"))
+    ttr = float(input("Entre a relação Type-Token:"))
+    hlr = float(input("Entre a Razão Hapax Legomana:"))
+    sal = float(input("Entre o tamanho médio de sentença:"))
+    sac = float(input("Entre a complexidade média da sentença:"))
+    pal = float(input("Entre o tamanho medio de frase:"))
 
     return [wal, ttr, hlr, sal, sac, pal]
 
@@ -26,6 +26,11 @@ def le_textos():
 
     return textos
 
+
+#####################################################
+#############################################################################
+#############################################################################
+#############################################################################
 def separa_sentencas(texto):
     '''A funcao recebe um texto e devolve uma lista das sentencas dentro do texto'''
     sentencas = re.split(r'[.!?]+', texto)
@@ -68,25 +73,36 @@ def n_palavras_diferentes(lista_palavras):
             freq[p] = 1
 
     return len(freq)
+#############################################################################
+#############################################################################
+#############################################################################
+
+
 
 def compara_assinatura(as_a, as_b):
     '''IMPLEMENTAR. Essa funcao recebe duas assinaturas de texto e deve devolver o grau de similaridade nas assinaturas.'''
-    pass
+    sub_ass = 0
+    for i in range(6):
+        sub_ass += abs(as_a[i]) - abs(as_b[i])
+    grau_similaridade = sub_ass/6
+    return grau_similaridade
+        
 
 def calcula_assinatura(texto):
     '''IMPLEMENTAR. Essa funcao recebe um texto e deve devolver a assinatura do texto.'''
-    #Tamanho medio das palavras
+    
+    #obtendo alguns parametros
     tamanho_sentenca = len(separa_sentencas(texto))
     tamanho_palavra = 0
     palavras = separa_palavras (texto)
     total_palavras = len(palavras)
     frases = len(separa_frases(texto))
-
+    
+    #Tamanho medio das palavras
     for palavra in palavras:
         tamanho_palavra += len(palavra)
     tamanho_medio_palavras = tamanho_palavra / total_palavras
-    print("tamanho medio palavras: ",tamanho_medio_palavras)
-
+  
     #Calcula o type_token
     palavras_unicas = n_palavras_unicas(texto)
     type_token = palavras_unicas/total_palavras
@@ -101,18 +117,25 @@ def calcula_assinatura(texto):
     complexidade_sentenca = frases / tamanho_sentenca
     
     #tamanho medio da frase
-    print(frases)
     tamanho_medio_frase = tamanho_palavra 
+    
+    return [tamanho_medio_palavras, type_token, hapax, tamanho_palavra, complexidade_sentenca, tamanho_medio_frase]
 
 
 def avalia_textos(textos, ass_cp):
     '''IMPLEMENTAR. Essa funcao recebe uma lista de textos e uma assinatura ass_cp e deve devolver o numero (1 a n) do texto com maior probabilidade de ter sido infectado por COH-PIAH.'''
-    pass
+    ass = []
+    for i in textos:
+        ass.append(calcula_assinatura(i))
+    
+    for ass_ind in ass:
+        print(compara_assinatura(ass_ind, ass_cp))
 
-texto = "Então resolveu ir brincar com a Máquina pra ser também imperador dos filhos da mandioca. Mas as três cunhas deram muitas risadas e falaram que isso de deuses era gorda mentira antiga, que não tinha deus não e que com a máquina ninguém não brinca porque ela mata. A máquina não era deus não, nem possuía os distintivos femininos de que o herói gostava tanto. Era feita pelos homens. Se mexia com eletricidade com fogo com água com vento com fumo, os homens aproveitando as forças da natureza. Porém jacaré acreditou? nem o herói! Se levantou na cama e com um gesto, esse sim! bem guaçu de desdém, tó! batendo o antebraço esquerdo dentro do outro dobrado, mexeu com energia a munheca direita pras três cunhas e partiu. Nesse instante, falam, ele inventou o gesto famanado de ofensa: a pacova."
-#calcula_assinatura(texto)
-texto2 = "O gato caçava o rato"
 
-#print(separa_sentencas(texto2))
-
-calcula_assinatura(texto)
+def main():
+    ass_cp = le_assinatura()
+    list_ass = []
+    textos = le_textos() #retorna lista de textos
+    avalia_textos(textos, ass_cp)
+    
+main()
